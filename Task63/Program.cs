e8eef94391c9b9580472f6a14a72f62501b0f0e3
Artem <grekartemvl@mail.ru>
2023-01-31 18:48:12 +0500

    Add Task63

---
+++ Task63/Program.cs
@@ -0,0 +1,22 @@
+﻿/* 63. Задайте значение N. Напишите программу, которая выведет
+все натуральные числа в промежутке от 1 до N.   */
+int getFromUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+int userNumber = getFromUserData("Введите значение границы ряда");
+printRange(userNumber);
+void printRange(int N)
+{
+    if (N == 0)
+    {
+        return;
+    }
+    printRange(N - 1);
+    Console.Write(N + ",");
+   
+}
