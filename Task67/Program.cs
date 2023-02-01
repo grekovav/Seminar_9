02e09ab7e5db45f817adf0543bd9b830417ea73d
Artem <grekartemvl@mail.ru>
2023-02-01 12:51:38 +0500

    Add Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,21 @@
+﻿/* 67. Напишите программу, которая будет принимать на вход
+число и возвращать сумму его цифр.    */
+int getFromUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+int getSumOfDigits(int number)
+{
+    if (number == 0)
+    {
+        return 0;
+    }
+    return number % 10 + getSumOfDigits(number / 10);
+}
+int num = getFromUserData("Введите число");
+int result = getSumOfDigits(num);
+Console.WriteLine(result);
