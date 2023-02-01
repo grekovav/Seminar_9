610b2069818fc347194103847319beeaa99748b8
Artem <grekartemvl@mail.ru>
2023-02-01 13:05:05 +0500

    Add Task69

---
+++ Task69/Program.cs
@@ -0,0 +1,22 @@
+﻿/* 69. Напишите программу, которая на вход принимает два числа
+А и В, и возводит число А в степень В с помощью рекурсии.   */
+int getFromUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+int getRaiseDegree(int numA, int numB)
+{
+    if (numB == 1)
+    {
+        return numA;
+    }
+    return numA * getRaiseDegree(numA, numB - 1);
+}
+int numA = getFromUserData("Введите число A");
+int numB = getFromUserData("Введите число B");
+int result = getRaiseDegree(numA, numB);
+Console.WriteLine(result);
